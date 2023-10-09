15c52476f6a01663c8bca4edb375dd4a2046c982
Dean Wagner <dean@dfwagners.net>
2023-10-09 17:33:56 +0000

    camera movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Experience } from "./exp/Experience";
 import { HeadsUp } from "./components/headsup/HeadsUp.jsx"
 import { Menu } from './components/menu/Menu.jsx'
+// import { Camera } from './components/Camera'
 
 import * as React from 'react';
 import { appMachine } from './appMachine.js'
 import { createActorContext } from '@xstate/react'
+
+import { useControls } from "leva";
+
 
 export const AppContext = createActorContext(appMachine)
 
@@ -14,7 +18,7 @@ function App() {
   
   return (
     <AppContext.Provider>
-      <Canvas shadows camera={{ position: [0, 4, 10], fov: 30 }}>
+      <Canvas shadows camera={{ position: [4, 0, 10], fov: 30 }}>
         <color attach="background" args={["#ececec"]} />
         <Experience />
       </Canvas>
--- src/exp/Experience.jsx
+++ src/exp/Experience.jsx
@@ -1,10 +1,13 @@
+import { useThree } from "@react-three/fiber";
 import { Environment, OrbitControls, Sky, ContactShadows } from "@react-three/drei";
 import { Cube } from '../models/Cube.jsx'
 import { Knight } from '../models/knight/Knight.jsx'
 import { useControls } from "leva";
+import { useEffect, Vector3 } from "react";
 
 export const Experience = () => {
-  const {animation, headFollow, cursorFollow, wireframe} = useControls({
+  
+  const {animation, headFollow, cursorFollow, wireframe, Camx} = useControls({
     animation: {
       value: "Run",
       options: [
@@ -21,10 +24,25 @@ export const Experience = () => {
     headFollow: false,
     cursorFollow: false,
     wireframe: false,
+    Camx: 5,
   })
+
+  const {camera} = useThree();
+  console.log(camera)
+
+  useEffect(() => {
+    console.log(camera)
+    if (camera) {
+      camera.position.set(Camx, 2, 10);
+      camera.lookAt(0,0,0)
+    }
+    
+    
+  }, [Camx])
+
   return (
     <>
-      <OrbitControls />
+      {/* <OrbitControls /> */}
       <Cube position={[ -1, 0.5, -2]} tag="Joe" fsm="shapeRef1" friend="shapeRef2"/>
       <Cube position={[ 1, 0.5, -2]} tag = "fart" fsm="shapeRef2" friend="shapeRef1"/>
       <Sky />
